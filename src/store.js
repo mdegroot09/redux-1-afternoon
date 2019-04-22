cb496d7697c518aec0d8d78d8e58a3cc766a3a4e
Mike De Groot <mdegroot09@gmail.com>
2019-04-22 13:46:56 -0600

    initial commit

--- src/store.js
+++ src/store.js
@@ -1,1 +1,12 @@
+import {createStore} from 'redux'
 
+let initialState = {}
+
+reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state
+  }
+}
+
+export default createStore(reducer)
